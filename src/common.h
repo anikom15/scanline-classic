c8d6fa5794a6c659d1c9b45b244c2410ab68d60b
Ivan M. Martinez <anikom15@gmail.com>
2023-04-24 17:06:50 -0700

    Add common colors.

--- src/common.h
+++ src/common.h
@@ -18,6 +18,9 @@
 const float PI = 3.14159265359;
 const vec3 BLACK = vec3(0.0, 0.0, 0.0);
 const vec3 WHITE = vec3(1.0, 1.0, 1.0);
+const vec3 RED = vec3(1.0, 0.0, 0.0);
+const vec3 GREEN = vec3(0.0, 1.0, 0.0);
+const vec3 BLUE = vec3(0.0, 0.0, 1.0);
 
 float sq(const float x)
 {
